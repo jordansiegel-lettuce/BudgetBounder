ed3cc6f6a79489feafa611ff5c3c06deb5ac95ea
jordansiegel-lettuce <jordisiegel@gmail.com>
2026-04-15 12:13:11 +0300

    1504 class

---
+++ BudgetBounder/BudgetBounder.API/BudgetBounder.Api/Controllers/SavingGoalController.cs
@@ -0,0 +1,47 @@
+﻿using BudgetBounder.Api.Data;
+using BudgetBounder.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetBounder.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SavingGoalsController : ControllerBase
+    {
+        private readonly BudgetBounderDbContext _context;
+
+        public SavingGoalsController(BudgetBounderDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<SavingGoal>> GetSavingGoals()
+        {
+            return _context.SavingGoals.ToList();
+        }
+
+        [HttpPost]
+        public ActionResult<SavingGoal> CreateSavingGoal(SavingGoal goal)
+        {
+            _context.SavingGoals.Add(goal);
+            _context.SaveChanges();
+            return goal;
+        }
+
+        [HttpGet("user/{userId}")]
+        public ActionResult<List<SavingGoal>> GetUserGoals(int userId)
+        {
+            var userSavingGoals = _context.SavingGoals
+                .Where(t => t.UserId == userId)
+                .ToList();
+
+            return userSavingGoals;
+        }
+        [HttpPut("saving goals/{id}/progress")]
+        public ActionResult<List<SavingGoal>> GetSavingProgress()
+        {
+            
+        }
+    }
+}
