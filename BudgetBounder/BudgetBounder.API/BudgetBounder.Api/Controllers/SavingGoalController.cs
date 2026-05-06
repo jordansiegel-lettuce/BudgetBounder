e26eaad189768637efb492c22671219035917115
jordansiegel-lettuce <jordisiegel@gmail.com>
2026-05-06 11:06:10 +0300

    k

--- BudgetBounder/BudgetBounder.API/BudgetBounder.Api/Controllers/SavingGoalController.cs
+++ BudgetBounder/BudgetBounder.API/BudgetBounder.Api/Controllers/SavingGoalController.cs
@@ -43,13 +43,34 @@ namespace BudgetBounder.Api.Controllers
         public ActionResult<SavingGoal> UpdateSavingProgress(int id, double amountToAdd)
         {
             var goal = _context.SavingGoals.FirstOrDefault(g => g.Id == id);
-            if (goal==null)
-            return NotFound();
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            if (amountToAdd <= 0)
+            {
+                return BadRequest("Amount to add must be greater than 0.");
+            }
+
+            bool wasCompleted = goal.IsCompleted;
 
             goal.CurrentAmount += amountToAdd;
 
             if (goal.CurrentAmount >= goal.TargetAmount)
+            {
                 goal.IsCompleted = true;
+            }
+
+            if (!wasCompleted && goal.IsCompleted)
+            {
+                var user = _context.Users.FirstOrDefault(u => u.Id == goal.UserId);
+
+                if (user != null)
+                {
+                    user.XP += 100;
+                }
+            }
 
             _context.SaveChanges();
             return goal;
