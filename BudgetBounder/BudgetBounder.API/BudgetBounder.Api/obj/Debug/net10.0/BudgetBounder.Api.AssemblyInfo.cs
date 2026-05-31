c6371ec3ece2f3bbf3bda10ac48ab23e95a83d31
jordansiegel-lettuce <jordisiegel@gmail.com>
2026-05-31 11:40:31 +0300

    d

--- BudgetBounder/BudgetBounder.API/BudgetBounder.Api/obj/Debug/net10.0/BudgetBounder.Api.AssemblyInfo.cs
+++ BudgetBounder/BudgetBounder.API/BudgetBounder.Api/obj/Debug/net10.0/BudgetBounder.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetBounder.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26eaad189768637efb492c22671219035917115")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4e838dfb7eb98a1aebaadb2b752f71265b9e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetBounder.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetBounder.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
